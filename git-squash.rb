bbd7cc8c4b0c439c034466614b4de32d98d81d9e
Adam Stankiewicz <sheerun@sher.pl>
2020-06-01 18:08:46 +0200

    Fix formula

--- git-squash.rb
+++ git-squash.rb
@@ -1,5 +1,5 @@
 class GitSquash < Formula
-  desc "Squash commits on branch just like GitHub's "Squash and merge""
+  desc "Squash commits on branch just like GitHub's 'Squash and merge'"
   homepage "https://github.com/sheerun/git-squash"
   url "https://github.com/sheerun/git-squash/archive/v1.0.0.tar.gz"
   sha256 "1725ae80b10694d3bd0227768fecc69498b9ddd5cf129256a80c910b5f496f59"
