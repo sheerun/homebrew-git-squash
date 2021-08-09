de326a013ce522eac890cfad1220aaa3e651f88e
Adam Stankiewicz <sheerun@sher.pl>
2021-08-09 22:34:44 +0200

    Bump

--- git-squash.rb
+++ git-squash.rb
@@ -1,8 +1,8 @@
 class GitSquash < Formula
   desc "Squash commits on branch just like GitHub's 'Squash and merge'"
   homepage "https://github.com/sheerun/git-squash"
-  url "https://github.com/sheerun/git-squash/archive/v1.0.1.tar.gz"
-  sha256 "6ccaece4c3e93c0d6355a85ba1fc09151f81eca4693c58f79709d2301a9d9629"
+  url "https://github.com/sheerun/git-squash/archive/v1.0.2.tar.gz"
+  sha256 "4244e9b5c77f8162794077afbb3774b7574c2875f04903b3e3ba9141d982f953"
   head "https://github.com/sheerun/git-squash.git"
 
   def install
